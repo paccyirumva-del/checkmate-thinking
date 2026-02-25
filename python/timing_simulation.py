bb0f9b7d040d280b80b60b2b5f712b4d0caabedd
paccyirumva-del <paccyirumva@gmail.com>
2026-02-25 13:06:23 +0200

    Delete python directory

--- python/timing_simulation.py
+++
@@ -1,8 +0,0 @@
-# Simple timing example inspired by chess tempo
-def decide_action(time_left, complexity):
-    if time_left > complexity:
-        return "Proceed with action"
-    else:
-        return "Pause and reassess"
-
-print(decide_action(5, 3))
